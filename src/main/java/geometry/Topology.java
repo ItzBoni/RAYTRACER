251a6940a1352100a957de3b1ff5963f0a7f23f8
Santiago Bonilla <112128287+ItzBoni@users.noreply.github.com>
2026-04-29 18:42:44 -0600

    V0.3: Starting objreader

---
+++ src/main/java/geometry/Topology.java
@@ -0,0 +1,16 @@
+package geometry;
+
+import tools.Intersection;
+import tools.Ray;
+
+public class Topology extends Object3D {
+
+    @Override
+    public Intersection intersect(Ray ray){
+        Intersection i;
+
+
+
+        return i;
+    }
+}
---
+++ src/main/java/tools/ObjReader.java
@@ -0,0 +1,8 @@
+package tools;
+
+import java.io.BufferedReader;
+
+public class ObjReader {
+    private BufferedReader reader;
+
+}
